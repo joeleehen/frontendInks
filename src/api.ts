d4dcc8e57fdd88dc7517664f498dacf241252a5a
joseph <joeylhendrix@gmail.com>
2026-01-07 19:04:41 -0600

    typo fix: query proper prod endpoint instead of localhost

--- src/api.ts
+++ src/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     // baseURL: "http://localhost:8000"
-    baseUrl: "http://18.190.95.191:80"
+    baseURL: "http://18.190.95.191:80"
 });
 
 export default api;
