1aeb0e627c21d41a45e05ba1052a0de5de8197d6
joseph <joeylhendrix@gmail.com>
2026-01-07 19:02:56 -0600

    query aws lightsail prod endpoint instead of localhost

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "http://localhost:8000"
+    // baseURL: "http://localhost:8000"
+    baseUrl: "http://18.190.95.191:80"
 });
 
 export default api;
